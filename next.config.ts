ba4f0f4d53b1cacc3ed41b534986fa62dcce36ae
jichen <jichenssg@gmail.com>
2025-09-19 15:09:41 +1000

    Update .gitignore and package.json; configure turbopack and set dev port

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: '/home/jichen/code/jomety/home'
+  },
+	allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
 };
 
 export default nextConfig;
